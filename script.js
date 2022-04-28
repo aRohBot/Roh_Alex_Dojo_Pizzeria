47057092163aed6a578f8a5358a67553abad8aca
Alex Roh <imajndao@gmail.com>
2022-04-28 13:51:18 -0700

    added random pizza generator

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+var crustType = [
+    'deepdish',
+    'hand tossed',
+    'thin crust',
+    'stuffed crust',
+]
+var sauceType =[
+    'traditional',
+    'marinara',
+    'barbeque',
+    'alfredoo'
+]
+var cheeses = [
+    'mozzarella',
+    'pepperjack',
+    '5 cheese',
+    'feta',
+    'american'
+]
+var toppings = [
+    'pepperoni',
+    'sausage',
+    'mushrooms',
+    'olives',
+    'onions',
+    'chicken',
+    'peppers',
+    'pineapple',
+    'ham'
+]
+
+function randomizer(arr){
+    var i = Math.floor(arr.length * Math.random());
+    return arr[i];
+}
+
+function randomRange(min, max){
+    return Math.floor(Math.random() * max - min) + min;
+}
+
+function randomPizza(){
+    var pizza = {};
+    pizza.crustType = randomizer(crustType);
+    pizza.sauceType = randomizer(sauceType);
+    pizza.cheeses = [];
+    pizza.toppings = [];
+    for(var i = 0; i<randomRange(1,5); i++){
+        pizza.cheeses.push(randomizer(cheeses));
+    }
+    for(var i = 0; i<randomRange(1,6); i++){
+        pizza.toppings.push(randomizer(toppings));
+    }
+    return pizza;
+}
+
 function pizzaOven(crustType, sauceType, cheeses, toppings) {
     var pizza = {};
     pizza.crustType = crustType;
@@ -22,3 +77,7 @@ console.log(p3);
 var p4 = pizzaOven("stuffed crust", "barbeque", ["pepperjack"], ["chicken", "onions", "peppers"])
 console.log("pizza 4 is:");
 console.log(p4);
+
+var p5 = randomPizza();
+console.log("pizza 5 was randomly created:");
+console.log(p5);
